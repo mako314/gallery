ae0407429136d956728b77111c1b90a37cae0467
DuMark Allman <dumark.allman@yahoo.com>
2023-06-28 00:29:29 -0400

    {I still feel lost]

--- index2.js
+++ index2.js
@@ -17,3 +17,18 @@ fetch(url, {
    .then(response => response.json())
    .then(response => console.log(response))
 
+//Trying to figure out how to display my json on the DOM
+const renderHeroList = heroes => {
+    const li = document.createElement('li')
+    const h3 = document.createElement('h3')
+    const hName = document.createElement('p')
+    const img = document.createElement('img')
+    const btn = document.createElement('button')
+    
+    h3.textContent = heroes.hName
+    hName.textContent = 'Name: ${heroes.name}'
+    btn.textContent = 'Like'
+
+    img.src = heroes.imageUrl
+    li.className = 'list-li'
+}
