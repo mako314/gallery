32349c924907e534586867d7cd9aee06efc2f738
DuMark Allman <dumark.allman@yahoo.com>
2023-07-12 00:33:23 -0400

    {I still can't get the JSON to show the data!}

--- index2.js
+++ index2.js
@@ -17,9 +17,9 @@ fetch(url, {
    .then(response => response.json())
    .then(response => console.log(response))
 
-   function Error(error){
-    if (error.message === "")
-   }
+   //function Error(error){
+    //if (error.message === "")
+   
 
 //Trying to figure out how to display my json on the DOM
 const renderHeroList = heroes => {
@@ -35,4 +35,30 @@ const renderHeroList = heroes => {
 
     img.src = heroes.imageUrl
     li.className = 'list-li'
+
+    //Event listeners will be for removal till I figure out adding likes.
+    btn.addEventListener('click', () =>li.remove())
+
+    li.append(h3, img, btn)
+    document.querySelector('#heroes-list').append(li)
 }
+// Event handlers 
+function handleForm(e){
+    e.preventDefault()
+    //Supposed To Build Hero List
+    const hero = {
+        name: e.target.name.value,
+        imageUrl:e.target.imageUrl.value,
+        Likes: e.target.likes.value,
+        myFavorite: e.target.myFavorite.value
+    }
+    //render heroList(hero)
+}
+
+//Invoking functions
+//renderHeader()
+//renderFooter()
+//..forEach()
+//document.querySelector('#hero-list').addEventListener('submit', handleForm)
+
+
